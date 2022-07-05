53d8a711d64a82f0cb0fb0952a71687e4257fadd
andywangpt <95507674+andywangpt@users.noreply.github.com>
2022-07-05 16:50:20 -0700

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { useState } from "react"
+
+export default function Header(props) {
+
+    return (
+        <>
+        <h1>Expense Tracker</h1>
+        </>
+    )
+}
